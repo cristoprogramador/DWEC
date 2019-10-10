920d0b07b46ccf89a7b984d85db5158ebc0116d3
Cristóbal Salido Rodríguez <39799281+cristoprogramador@users.noreply.github.com>
2019-10-10 20:29:27 +0200

    Fin sudoku

--- Sudoku.jsx
+++ Sudoku.jsx
@@ -62,23 +62,64 @@ function utf8readline() {
 
 
 // AQUI EMPIEZA LA RESPUESTA DEL ALUMNO
-var miArrayBi= [[1,2,3,4,5,6,7,8,9],[1,2,3,1,2,3,1,2,3]]
+var miArrayBi= [[4, 1, 3, 8, 2, 5, 6, 7, 9],[5, 6, 7, 1, 4, 9, 8, 3, 2],[2, 8, 9, 7, 3, 6, 1, 4, 5],[1, 9, 5, 4, 6, 2, 7, 8, 3],
+[7, 2, 6, 9, 8, 3, 5, 1, 4],[3, 4, 8, 5, 1, 7, 2, 9, 6],[8, 5, 1, 6, 9, 4, 3, 2, 7],[9, 7, 2, 3, 5, 8, 4, 6, 1],[6, 3, 4, 2, 7, 1, 9, 5, 8]]
 print(esSudokuCorrecto(miArrayBi));
 
 function esSudokuCorrecto(miArrayBi)
 {
+    if(miArrayBi.length < 9)
+    {
+        return false;
+    }
     for(var i = 0; i < 9; i++)
-    {   
-        var num = new Array();     
+    {  
+        if(miArrayBi[i].length < 9)
+        {
+            return false;
+        }
+        var num = new Array(); 
+        var num2 = new Array();    
         for(var j = 0; j < 9; j++)
-        {            
-            num[j] = miArrayBi[i][j]
-
-            for(var k = 1; k < num.length; k++)
+        { 
+            if (num.includes(miArrayBi[i][j]))
             {
-                if (num[j] == num[k])
-                {return false}
+                return false;
+            }
+            else
+            {
+                num[j] = miArrayBi[i][j]
+            }
+
+            if (num2.includes(miArrayBi[j][i]))
+            {
+                return false;
+            }
+            else
+            {
+                num2[i] = miArrayBi[j][i]
             }
         }        
     }
+
+    for(var i = 0; i < 9; i += 3)
+    {   
+        var num = new Array();    
+        for(var j = 0+i; j < 3+i; j++)
+        { 
+            for(var k = 0+i; k < 3+i; k++)
+            { 
+                if (num.includes(miArrayBi[j][k]))
+                {
+                    return false;
+                }
+                else
+                {
+                    num[j] = miArrayBi[j][k]
+                }               
+            }  
+        }     
+    }
+
+    return true;
 }
